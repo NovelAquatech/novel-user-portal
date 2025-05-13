e05fcbb647e7fbe444a7c916d8c4b1a69fbef47e
Somdotta Sarkar <jhilick.s@gmail.com>
2025-05-13 09:22:09 +0200

    Updated msg and loading state for analysis report

--- src/components/detailed_analytics.jsx
+++ src/components/detailed_analytics.jsx
@@ -7,12 +7,14 @@ import { jsPDF } from "jspdf";
 import { useState, useEffect } from "react";
 import { APP_CONST } from "../helper/application-constant";
 import { useAuth } from "../hooks/useAuth";
-import { getOrganizedSensorData, organizedExportedData, capitalizeFirstLetter } from "../helper/utils";
+import {
+  getOrganizedSensorData,
+  organizedExportedData,
+  capitalizeFirstLetter,
+} from "../helper/utils";
 import { downloadExcel } from "../helper/download-utils";
 import { CirclesWithBar } from "react-loader-spinner";
-import {
-  getSensorData,
-} from "../helper/web-service";
+import { getSensorData } from "../helper/web-service";
 export const DetailedAnalytics = React.forwardRef(
   (
     {
@@ -23,11 +25,10 @@ export const DetailedAnalytics = React.forwardRef(
       selectedParam,
       setSelectedHourly,
       setSelectedParam,
-      machines
+      machines,
     },
     ref
   ) => {
-
     let displayParameters = [];
     Object.keys(parameters).map((parameter, i) => {
       displayParameters.push(parameter);
@@ -38,6 +39,7 @@ export const DetailedAnalytics = React.forwardRef(
     const [organizedSerieData, setOrganizedSerieData] = useState([]);
     const [isLoaderVisible, setLoaderVisible] = useState(false);
     const [sensorData, setSensorData] = useState([]);
+    const [dataLoaded, setDataLoaded] = useState(false);
     // Updated function
     const detailedAnalyticsData = () => {
       let pdt = moment().add(-1, "hours");
@@ -57,10 +59,12 @@ export const DetailedAnalytics = React.forwardRef(
         pdt = timeRanges[selectedHourly];
       }
 
-      
       // Determine the data range based on the selected hourly filter
-      const { seriesData } = getOrganizedSensorData(sensorData, Object.keys(parameters));
-       series = seriesData;
+      const { seriesData } = getOrganizedSensorData(
+        sensorData,
+        Object.keys(parameters)
+      );
+      series = seriesData;
 
       let sd = [];
       devices.forEach((device) => {
@@ -75,7 +79,6 @@ export const DetailedAnalytics = React.forwardRef(
           return;
         }
 
-
         // Process data for each selected parameter
         selectedParam.forEach((pm, index) => {
           let xArr = [];
@@ -84,24 +87,25 @@ export const DetailedAnalytics = React.forwardRef(
           // yaxis for mutiple yaxis
           let yaxis = `y`;
           let prm = parameters[pm.value];
-                   if (index > 0) {
-            yaxis = `y${index + 1}`
+          if (index > 0) {
+            yaxis = `y${index + 1}`;
             layout[`yaxis${index + 1}`] = {
               title: {
                 text: `${pm.label} (${prm?.unit ?? ""})`,
-                font: { size: 10 }
+                font: { size: 10 },
               },
-              "anchor": "free",
-              "side": "left",
-              "position": 0.05 * index,
-              "overlaying": 'y'
+              anchor: "free",
+              side: "left",
+              position: 0.05 * index,
+              overlaying: "y",
             };
           } else {
-            layout["yaxis"]["title"]["text"] = `${pm.label} (${prm?.unit ?? ""})`;
+            layout["yaxis"]["title"]["text"] = `${pm.label} (${
+              prm?.unit ?? ""
+            })`;
           }
           // Set xaxis domain
           layout["xaxis"]["domain"] = [0.05 * selectedParam.length, 1];
-
 
           data.forEach((s) => {
             let cdt = moment(s.timestamp);
@@ -118,25 +122,30 @@ export const DetailedAnalytics = React.forwardRef(
 
           // Add the series to the chart data if there is data to display
           if (xArr.length > 0 && yArr.length > 0) {
-
-            if (parameters[param].paramDisplayName === 'Power') { 
-              if (!machines.some(machine => machine.primaryDevEUI === device.devEUI)) {
-                  return;
+            if (parameters[param].paramDisplayName === "Power") {
+              if (
+                !machines.some(
+                  (machine) => machine.primaryDevEUI === device.devEUI
+                )
+              ) {
+                return;
               }
-          }
-            
+            }
+
             let machineName = `${device.devName} - ${parameters[param]?.paramDisplayName}`;
-          
+
             if (machines && machines.length > 0) {
-              const matchedMachine = machines.find(machine =>
-                machine.devEUIs && JSON.parse(machine.devEUIs).includes(device.devEUI)
+              const matchedMachine = machines.find(
+                (machine) =>
+                  machine.devEUIs &&
+                  JSON.parse(machine.devEUIs).includes(device.devEUI)
               );
-          
+
               if (matchedMachine) {
                 machineName = `${matchedMachine.PartitionKey} - ${device.devName}: ${parameters[param]?.paramDisplayName}`;
               }
             }
-          
+
             sd.push({
               x: xArr,
               y: yArr,
@@ -146,14 +155,16 @@ export const DetailedAnalytics = React.forwardRef(
               line: { width: 1 },
               name: machineName,
             });
-          }          
+          }
         });
       });
 
       // Add overall people present data if selected
       let cumulativePeoplePresentdata =
         series[APP_CONST.overAllPeoplePresentDevEUIKey] ?? null;
-      let filterParam = selectedParam.filter((param) => param.value === "people_present");
+      let filterParam = selectedParam.filter(
+        (param) => param.value === "people_present"
+      );
       if (filterParam.length > 0 && cumulativePeoplePresentdata) {
         let xArr = [];
         let yArr = [];
@@ -186,6 +197,7 @@ export const DetailedAnalytics = React.forwardRef(
       }
 
       setOrganizedSerieData(sd);
+      setDataLoaded(true);
     };
 
     useEffect(() => {
@@ -194,6 +206,7 @@ export const DetailedAnalytics = React.forwardRef(
 
     const fetchSensorData = async () => {
       setLoaderVisible(true);
+      setDataLoaded(false);
       try {
         const sensorDataResp = await getSensorData(user, selectedHourly);
         const sensorData = sensorDataResp?.["value"];
@@ -210,12 +223,10 @@ export const DetailedAnalytics = React.forwardRef(
     useEffect(() => {
       fetchSensorData();
     }, [selectedHourly]);
-    
 
     const handleHourlyFilterChange = (event) => {
       setSelectedHourly(event.target.value);
     };
-
 
     const { user } = useAuth();
     const farmer_companies = APP_CONST.farmer_companies;
@@ -275,7 +286,13 @@ export const DetailedAnalytics = React.forwardRef(
                 const hiddenDetails = [
                   { text: `Report Name: Detailed-Analysis`, x: 10, y: 20 },
                   { text: `Report Frequency: ${selectedHourly}`, x: 10, y: 30 },
-                  { text: `Report Type: ${selectedParam.map(item => item.label).join(', ')}`, x: 10, y: 40 },
+                  {
+                    text: `Report Type: ${selectedParam
+                      .map((item) => item.label)
+                      .join(", ")}`,
+                    x: 10,
+                    y: 40,
+                  },
                 ];
 
                 pdf.setFont("helvetica");
@@ -298,24 +315,31 @@ export const DetailedAnalytics = React.forwardRef(
       });
     };
 
-
     const downloadChartAsExcel = () => {
       return new Promise((resolve, reject) => {
         try {
           const columns = [
             { title: "Date", dataKey: "timestamp" },
             ...displayParameters.map((parameter) => {
-              return { title: capitalizeFirstLetter(parameter), dataKey: parameter }
-            })
+              return {
+                title: capitalizeFirstLetter(parameter),
+                dataKey: parameter,
+              };
+            }),
           ];
-          const filterData = organizedExportedData(sensorData, selectedHourly, parameters, selectedDevices);
+          const filterData = organizedExportedData(
+            sensorData,
+            selectedHourly,
+            parameters,
+            selectedDevices
+          );
           if (filterData) {
             downloadExcel(filterData, columns, devices, user);
           }
-          
+
           resolve();
         } catch (error) {
-          reject(error)
+          reject(error);
         }
       });
     };
@@ -323,7 +347,7 @@ export const DetailedAnalytics = React.forwardRef(
     React.useImperativeHandle(ref, () => ({
       downloadChartAsPDF,
       downloadChartAsExcel,
-      fetchSensorData
+      fetchSensorData,
     }));
 
     const sortedData = [...organizedSerieData].sort((a, b) => {
@@ -333,16 +357,18 @@ export const DetailedAnalytics = React.forwardRef(
     });
 
     const updateParamName = (paramDisplayName) => {
-      if(paramDisplayName.toLowerCase() == 'tvoc') return paramDisplayName.toUpperCase();
-      if(paramDisplayName.toLowerCase() == 'co2') return "CO₂";
-      return paramDisplayName; 
-    }
+      if (paramDisplayName.toLowerCase() == "tvoc")
+        return paramDisplayName.toUpperCase();
+      if (paramDisplayName.toLowerCase() == "co2") return "CO₂";
+      return paramDisplayName;
+    };
 
     const multiSelectOptions = displayParameters.map((parameter) => {
       return {
-        label: capitalizeFirstLetter(updateParamName(parameter)), value: parameter
-      }
-    })
+        label: capitalizeFirstLetter(updateParamName(parameter)),
+        value: parameter,
+      };
+    });
 
     return (
       <>
@@ -369,22 +395,19 @@ export const DetailedAnalytics = React.forwardRef(
               labelledBy="Select"
               hasSelectAll={false}
             />
-
-
           </div>
         </div>
 
         {/* Graph */}
-        { isLoaderVisible ?
+        {isLoaderVisible ? (
           <CirclesWithBar
-          color="#00bfff"
-          height="70"
-          width="70"
-          wrapperClass="loader-graph"
-          visible={isLoaderVisible}
-        />
-        : 
-        sortedData.length > 0 ? (
+            color="#00bfff"
+            height="70"
+            width="70"
+            wrapperClass="loader-graph"
+            visible={isLoaderVisible}
+          />
+        ) : !dataLoaded ? null : sortedData.length > 0 ? (
           <div id="chart-container">
             <Plot
               data={sortedData}
@@ -393,7 +416,7 @@ export const DetailedAnalytics = React.forwardRef(
                 displayModeBar: false,
               }}
               useResizeHandler={true}
-              style={{ width: "100%", marginLeft: '-15px' }}
+              style={{ width: "100%", marginLeft: "-15px" }}
             />
           </div>
         ) : (
@@ -402,12 +425,16 @@ export const DetailedAnalytics = React.forwardRef(
               paddingTop: "50px",
               paddingBottom: "80px",
               height: "410px",
+              fontSize: "15px",
             }}
           >
-            <b>No data found</b>
+            <b>
+              No data available for the selected time range. The devices were
+              offline during this period.
+              <p>Please choose a different time range to view the data.</p>
+            </b>
           </div>
         )}
-        
       </>
     );
   }
