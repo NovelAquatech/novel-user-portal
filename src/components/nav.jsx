6432ad22bd60986280fae50d8c9310381d6e59ea
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2025-05-30 21:30:36 +0530

    Quick fix

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -40,7 +40,7 @@ export const Navbar = () => {
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
           </button>
-          <a className="navbar-brand" href="/novel-dev/reports">
+          <a className="navbar-brand" href="/novel-dev/report">
             <img
               src={orgIcon}
               className={orgName == "UNSW" ? "orglog" : "orglog orglog2"}
